614648cc1783e692fa3325e235b0c1ace7870d81
Ivan Zaharuk <zaharuck@outlook.com>
2024-10-01 19:16:00 +0300

    Add tutor list item card draft component

---
+++ LearnIT/Frontend/LearnIT/DTOs/TutorDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace LearnIT.DTOs
+{
+    public class TutorDTO
+    {
+        public int Id { get; set; }
+
+        public UserDTO User { get; set; } = null!;
+
+        public List<string> Skills { get; set; } = null!;
+
+        public int? Rating { get; set; }
+
+        public int WagePerHour { get; set; }
+
+        public string JobTitle { get; set; } = null!;
+
+        public string SummaryOfQualification { get; set; } = null!;
+
+        public string? GitHubUrl { get; set; }
+
+        public string? LinkedInUrl { get; set; }
+
+        public string LogoUrl { get; set; } = null!;
+    }
+}
