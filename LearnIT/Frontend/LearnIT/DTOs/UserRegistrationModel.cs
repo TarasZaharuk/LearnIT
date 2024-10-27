ccbf6920061cf83972ecd84e78289a0e4f1e9327
TarasZaharuk <taraszaharuk410@gmail.com>
2024-10-27 12:33:12 +0100

    Add user registration dialog on the frontend side

---
+++ LearnIT/Frontend/LearnIT/DTOs/UserRegistrationModel.cs
@@ -0,0 +1,17 @@
+﻿namespace LearnIT.DTOs
+{
+    public class UserRegistrationModel
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Gender { get; set; } = null!;
+
+        public DateOnly BirthDate { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
